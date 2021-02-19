f18385be1b670e7c44d87dc980fefadec1ebac4e
sairish2001 <sairish2001@gmail.com>
2021-02-19 13:36:07 +0530

    Initial Commit

---
+++ calert_backend/calert_backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for calert_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calert_backend.settings')
+
+application = get_wsgi_application()
