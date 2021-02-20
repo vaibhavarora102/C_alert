36daec6e4b4d7950986f04c391f8b25c3181c0da
KetanAggarwal58 <ketanaggarwal58@gmail.com>
2021-02-20 22:56:02 +0530

    initial commit

---
+++ calert/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color white = Colors.white;
+const Color fadedWhite = Colors.white70;
+const Color cyan = Colors.cyan;
+const Color orange = Color(0xFFFF8F00);
+const Color black = Colors.black;
+const Color red = Colors.red;
+const Color blue = Colors.blue;
+const Color liteGreen = Color(0xFFA5D6A7);
